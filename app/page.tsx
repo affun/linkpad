b25871e6efb064b3cd1d6100185faad2f1d22ed0
Affun <affangv8@gmail.com>
2026-07-21 23:36:34 +0530

    feat: landing page started, basic components made

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Navbar from "@/app/components/landing/Navbar";
+import Hero from "@/app/components/landing/Hero";
+import Features from "@/app/components/landing/Features";
+import Templates from "@/app/components/landing/Templates";
+import Pricing from "@/app/components/landing/Pricing";
+import Footer from "@/app/components/landing/Footer";
 
 const Home = () => {
     return (
-        <div><h1 className={"text-4xl"}> LINKPAD</h1></div>
+        <div className={"container-page"}>
+            <Navbar/>
+            {/*<Hero/>*/}
+            {/*<Features/>*/}
+            {/*<Templates/>*/}
+            {/*<Pricing/>*/}
+            {/*<Footer/>*/}
+        </div>
     )
 }
 export default Home
